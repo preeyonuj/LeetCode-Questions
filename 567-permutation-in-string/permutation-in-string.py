64a54abee5d0449248f4a3ba11e574bfc80085df
Preeyonuj Boruah <44507962+preeyonuj@users.noreply.github.com>
2025-06-09 18:47:19 -0400

    Time: 3039 ms (21.39%) | Memory: 12.3 MB (99.68%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -6,11 +6,26 @@ class Solution(object):
         :rtype: bool
         """
         s1_len = len(s1)
+        s1_cnt = {}
 
-        for l in range(len(s2) - s1_len + 1):
-            # print(sorted(s2[l:l+s1_len]))
-            # print(sorted(s1))
-            if sorted(s2[l:l+s1_len]) == sorted(s1):
+        for i in s1:
+            s1_cnt[i] = 1 + s1_cnt.get(i, 0)
+
+        s2_cnt = {}
+        l = 0
+        r = l + s1_len
+
+        while r <= len(s2):
+            s2_cnt = {}
+            for i in s2[l:r]:
+                s2_cnt[i] = 1 + s2_cnt.get(i, 0)
+
+
+            if s1_cnt == s2_cnt:
                 return True
 
+            l += 1
+            r = l + s1_len
+
         return False
+            
